cdefb4559ba35b42152709590e94e1307206e280
awanjila <abemuchikan@gmail.com>
2016-10-31 01:21:29 +0300

    added, subclasses to demonstrate inheritance

--- propertyManager.py
+++ propertyManager.py
@@ -4,7 +4,12 @@ class Property(object):
 		self.size=size;
 		self.number=number
 		self.value=value
-	def buying:
+	def buy_property:
 		pass
-    def __str__(self):
-     return "%s is a %s" % (self.name, self.size, self.number, self.value)
+		
+	def __str__(self):
+    	return "%s is a %s" % (self.name, self.size, self.number, self.value)
+class House(Property):
+class Land(Property):
+class Car(Property):
+
