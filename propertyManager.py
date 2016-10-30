eb0ab2401366c09aa3e9ddfe8a569b9c8fd4102a
awanjila <abemuchikan@gmail.com>
2016-10-31 01:26:02 +0300

    adding methods to my property class

--- propertyManager.py
+++ propertyManager.py
@@ -4,9 +4,14 @@ class Property(object):
 		self.size=size;
 		self.number=number
 		self.value=value
-	def buy_property:
+	def buy_property(self):
+
 		pass
-		
+	def sell_property(self):
+		pass
+	def rent_property(self):
+		pass
+
 	def __str__(self):
     	return "%s is a %s" % (self.name, self.size, self.number, self.value)
 class House(Property):
