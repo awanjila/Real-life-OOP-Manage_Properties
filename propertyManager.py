474a7dd339aea63f1252eb9f7ea54fcafce88b4a
awanjila <abemuchikan@gmail.com>
2016-10-30 22:36:33 +0300

    my first commit

---
+++ propertyManager.py
@@ -0,0 +1,10 @@
+class Property(object):
+	def __init__(self):
+		self.name=name;
+		self.size=size;
+		self.number=number
+		self.value=value
+	def buying:
+		pass
+    def __str__(self):
+     return "%s is a %s" % (self.name, self.size, self.number, self.value)
