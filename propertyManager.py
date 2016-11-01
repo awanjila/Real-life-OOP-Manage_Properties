503f5f4e8e2901cf74625e3325c2d4bb49552cdc
awanjila <abemuchikan@gmail.com>
2016-11-02 00:59:39 +0300

    To demonstarte inheritance i have added three instances of the class property, that is land car and house instances

--- propertyManager.py
+++ propertyManager.py
@@ -5,12 +5,15 @@ class Property(object):
 		self.value=value
 		self.status=status
 	def get_property(self):
-		return self.name
+		return self.name + self.type_property
 
-	def buy_property(self):
+	def buy_property(self, amount):
+		self.value+=amount
+		self.status="bought"
 		
 	def sell_property(self):
- 		pass
+		self.value-=value;
+ 		self.status="sold"
 	def rent_property(self, amount):
 		if isinstance(Property, land):
 			self.value+=amount
@@ -18,10 +21,13 @@ class Property(object):
 			self.value=self.value+(12*amount)
 		return self.value				
     def add_property(self):
-		pass
+		
 
 	def __str__(self):
-		return "%s is a %s" % (self.name, self.type_property, self.number, self.value)
-house=Property()
-land=Property()
-car=Property
+		return "%s is a %s whose number is %s and has a value of %s" % (self.name, self.type_property, self.number, self.value)
+class House(Property):
+	pass
+class Land(Property):
+	pass
+class Car(Property):
+	pass
